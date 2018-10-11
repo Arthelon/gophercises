279f9d541ed77f57d4d17eee18e83ea0fe31cf8a
Daniel Hsing <hsing.daniel@gmail.com>
2018-10-10 23:14:39 -0400

    feat(urlshort): implement yamlhandler
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- urlshort/go.mod
+++ urlshort/go.mod
@@ -3,5 +3,6 @@ module github.com/arthelon/gophercises/urlshort
 require (
 	github.com/boltdb/bolt v1.3.1
 	github.com/gophercises/urlshort v0.0.0-20180528194359-211976381743
+	golang.org/x/sys v0.0.0-20181005133103-4497e2df6f9e // indirect
 	gopkg.in/yaml.v2 v2.2.1
 )
--- urlshort/handler.go
+++ urlshort/handler.go
@@ -2,16 +2,11 @@ package urlshort
 
 import (
 	"fmt"
+	yaml "gopkg.in/yaml.v2"
 	"net/http"
 	"strings"
 )
 
-// MapHandler will return an http.HandlerFunc (which also
-// implements http.Handler) that will attempt to map any
-// paths (keys in the map) to their corresponding URL (values
-// that each key in the map points to, in string format).
-// If the path is not provided in the map, then the fallback
-// http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(rw http.ResponseWriter, req *http.Request) {
 		path := strings.Trim(req.URL.EscapedPath(), " ")
@@ -25,23 +20,23 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 	}
 }
 
-// YAMLHandler will parse the provided YAML and then return
-// an http.HandlerFunc (which also implements http.Handler)
-// that will attempt to map any paths to their corresponding
-// URL. If the path is not provided in the YAML, then the
-// fallback http.Handler will be called instead.
-//
-// YAML is expected to be in the format:
-//
-//     - path: /some-path
-//       url: https://www.some-url.com/demo
-//
-// The only errors that can be returned all related to having
-// invalid YAML data.
-//
-// See MapHandler to create a similar http.HandlerFunc via
-// a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	// TODO: Implement this...
-	return nil, nil
+	records, err := parseYAML(yml)
+	if err != nil {
+		return fallback.ServeHTTP, err
+	}
+	return func(rw http.ResponseWriter, req *http.Request) {
+		path := strings.Trim(req.URL.EscapedPath(), " ")
+		for _, pathRecord := range records {
+			if pathRecord["path"] == path {
+				http.Redirect(rw, req, pathRecord["url"], http.StatusSeeOther)
+			}
+		}
+		fallback.ServeHTTP(rw, req)
+	}, nil
+}
+
+func parseYAML(yml []byte) (dst []map[string]string, err error) {
+	err = yaml.Unmarshal(yml, &dst)
+	return dst, err
 }
